a0b4be88f6c9f10b419aec5cc6efde3f0eaeca3c
Arshad Husain <husain3@ualberta.ca>
2018-08-25 17:21:57 -0600

    added ability to loop through all detected 'stopsigns' and draw rectangles around them

--- stopSignDetector.cc
+++ stopSignDetector.cc
@@ -12,7 +12,7 @@ using namespace cv;
 void detectAndDisplay(Mat frame);
 
 /* Global Variables */
-String stop_sign_cascade_name = "./classifer/cascade.xml"; //Will need to modify this for path or put this code in the same direct
+String stop_sign_cascade_name = "cascade.xml"; //Will need to modify this for path or put this code in the same direct
 CascadeClassifier stop_sign_cascade;
 String window_name = "Capture - Stop Sign Detection";
 
@@ -30,7 +30,7 @@ int main(int argc, const char ** argv)
 		return -1;
 	}
 
-	stop_sign_img = imread("stop-sign-pic.jpg");
+	stop_sign_img = imread("infiniti.png");
 	//imshow("manhan", stop_sign_img);
 
 	detectAndDisplay(stop_sign_img);
@@ -47,12 +47,18 @@ void detectAndDisplay(Mat frame)
 	equalizeHist(frame_gray, frame_gray);
 
 	cout << "Size: " << stopSigns.size() << '\n';
-	stop_sign_cascade.detectMultiScale(frame_gray, stopSigns, 1.1, 4, 0|CV_HAAR_SCALE_IMAGE, Size(300, 300));
+	stop_sign_cascade.detectMultiScale(frame_gray, stopSigns, 1.1, 3, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50));
 	cout << "Size: " << stopSigns.size() << '\n';
-	cout << stopSigns[0].x << " " << stopSigns[0].y << " " << stopSigns[0].width << " " << stopSigns[0].height << '\n';
+	//cout << stopSigns[0].x << " " << stopSigns[0].y << " " << stopSigns[0].width << " " << stopSigns[0].height << '\n';
 
-	cv::Rect rect(stopSigns[0].x, stopSigns[0].y, stopSigns[0].width, stopSigns[0].height);
-	cv::rectangle(frame, rect, cv::Scalar(0, 255, 0), 12);
+	//cv::Rect rect(stopSigns[0].x, stopSigns[0].y, stopSigns[0].width, stopSigns[0].height);
+	//cv::rectangle(frame, rect, cv::Scalar(0, 255, 0), 12);
+
+	for(std::vector<Rect>::size_type i = 0; i != stopSigns.size(); i++)
+	{
+		cv::Rect rect(stopSigns[i].x, stopSigns[i].y, stopSigns[i].width, stopSigns[i].height);
+		cv::rectangle(frame, rect, cv::Scalar(0, 255, 0), 12);
+	}
 	
 	//cv::namedWindow("stopsigndetect", WINDOW_NORMAL);
 	imshow("stopsigndetect", frame); //1024, 768
@@ -64,3 +70,5 @@ void detectAndDisplay(Mat frame)
 
 //TO BUILD: g++ `pkg-config --cflags --libs opencv` stopSignDetector.cc
 ///Volumes/Macintosh SD/arshadhusain/Desktop/Computer Vision and Autonomous Car Work/All Computer Vision Projects/traffic_signs/stop_sign_detection/opencv_cctraining_real/classifier
+
+
